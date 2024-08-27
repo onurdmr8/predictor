b39a010eeb6a38e9082d1a7467f8edbdb8adbf79
onurdmr8 <onurdmr0228@gmail.com>
2024-08-27 10:51:15 +0300

    okeeyletsgo

--- main.py
+++ main.py
@@ -1,46 +1,28 @@
 import time
 import requests
-import json
-from sklearn.preprocessing import MinMaxScaler
 from colorama import Fore, Style
-import warnings
 import yfinance as yf
 from datetime import datetime, timedelta
 import pandas as pd
-import random
 import re
 import numpy as np
-from sklearn.neighbors import KNeighborsRegressor
 from tabulate import tabulate
-from binance.client import Client
-from pmdarima import auto_arima
-import traceback
-import openpyxl
-import statsmodels.api as sm
-from sklearn.neural_network import MLPRegressor
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor, AdaBoostRegressor
 from sklearn.ensemble import VotingRegressor
-from sklearn.metrics import mean_squared_error
-import matplotlib.pyplot as plt
 from bs4 import BeautifulSoup
 from xgboost import XGBRegressor
 from lightgbm import LGBMRegressor
-from catboost import CatBoostRegressor
-from sklearn.svm import SVR
-from sklearn.linear_model import ElasticNet
-from sklearn.neighbors import KNeighborsRegressor
-from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import cross_val_score
+import streamlit as st
 
+st.set_page_config(layout="wide", page_title="Dorecast")
 api_key = 'RjCgL26lL8GoDagHA4Pb2wFC9414Oenhp5oGOfQMJrCJbWpU9yBtMPofuNAm3cXL'
 api_secret = 'SGNelarbzwvaFVRpQXhfeX9EPbLFRYIIKi8B2PloNTepvT6Q12LIYsCXbgkn8DGF'
 
-
 end = datetime.now() + timedelta(days=1)
-end = end.strftime("%Y-%m-%d")
 
-startdate = datetime.now() - timedelta(days=999)
+
+startdate = datetime.now() - timedelta(days=41)
 startdate = startdate.strftime("%Y-%m-%d")
 
 def send_telegram_message(message):
@@ -130,46 +112,50 @@ def crypto():
     return cr
 
 cr=crypto()
-liste=cr+bist
+liste=cr
 print(str("kripto: "+str(len(cr))))
 print(str("bist: "+str(len(bist))))
-def newthe(liste):
+def newthe(liste,end=end):
     sepet = pd.DataFrame(columns=["symbol","close","difference",
                                   "forecast","date",
                                   "hedef_tarih","MAPE"])
-    süre=7
+    interval='1h'
+
+
+    süre=24*3
     beklenen_sinyal=1
     sell=pd.DataFrame(columns=["symbol","close","date"])
     say1 = 0
     for symbol in liste:
-        say1 += 1
-
-        if symbol.endswith(".IS"):
-            beklenen_degisim = 15
-        else:
-            beklenen_degisim = 20
-        print(Fore.CYAN + str(say1)+"/"+str(len(liste))+" "+symbol+" "+ Style.RESET_ALL)
         try:
+            say1 += 1
 
-            data = yf.download(symbol, start=startdate, progress=False, interval='1d', end=end)
+
+            if symbol.endswith(".IS"):
+                beklenen_degisim = 5
+            else:
+                beklenen_degisim = 100
+            print(Fore.CYAN + str(say1)+"/"+str(len(liste))+" "+symbol+" "+ Style.RESET_ALL)
+
+            data = yf.download(symbol,threads=True,repair=True,start=startdate, progress=False, interval=interval, end=end)
             data['close'] = data['Close']
+            data.reset_index(inplace=True)
             ortalama = data['close'].mean()
 
-            son_tarih=datetime.now()-timedelta(days=6)
+            data['Datetime'] = pd.to_datetime(data['Datetime']).dt.tz_localize(None)
 
 
-            if son_tarih < data.index[-1] < datetime.now():
-                pass
-            else:
-                print("tarih hatası")
-                print(data.index[-1])
-                continue
+
+
+
+            # Teknik göstergeleri hesapla
             def calculate_macd(data, short_window=12, long_window=26, signal_window=9):
                 data['EMA12'] = data['close'].ewm(span=short_window, adjust=False).mean()
                 data['EMA26'] = data['close'].ewm(span=long_window, adjust=False).mean()
                 data['MACD'] = data['EMA12'] - data['EMA26']
                 data['Signal Line'] = data['MACD'].ewm(span=signal_window, adjust=False).mean()
                 data['MACD Signal'] = np.where(data['MACD'] > data['Signal Line'], 1, -1)
+
             def calculate_rsi(data, window=14):
                 delta = data['close'].diff()
                 gain = (delta.where(delta > 0, 0)).rolling(window=window).mean()
@@ -177,33 +163,36 @@ def newthe(liste):
                 rs = gain / loss
                 data['RSI'] = 100 - (100 / (1 + rs))
                 data['RSI Signal'] = np.where(data['RSI'] < 30, 1, np.where(data['RSI'] > 70, -1, 0))
-            def calculate_sma(data, short_window=50, long_window=200):
-                data['SMA50'] = data['close'].rolling(window=short_window).mean()
-                data['SMA200'] = data['close'].rolling(window=long_window).mean()
-                data['SMA Signal'] = np.where(data['SMA50'] > data['SMA200'], 1, -1)
-            def calculate_bollinger_bands(data, window=20):
-                data['SMA20'] = data['close'].rolling(window=window).mean()
-                data['BB_upper'] = data['SMA20'] + 2 * data['close'].rolling(window=window).std()
-                data['BB_lower'] = data['SMA20'] - 2 * data['close'].rolling(window=window).std()
-                data['BB Signal'] = np.where(data['close'] > data['BB_upper'], -1,
-                                             np.where(data['close'] < data['BB_lower'], 1, 0))
+
+            def calculate_kdj(data, window=14):
+                low_min = data['Low'].rolling(window=window).min()
+                high_max = data['High'].rolling(window=window).max()
+                data['%K'] = 100 * ((data['close'] - low_min) / (high_max - low_min))
+                data['%D'] = data['%K'].rolling(window=3).mean()
+                data['%J'] = 3 * data['%K'] - 2 * data['%D']
+                data['KDJ Signal'] = np.where(data['%J'] < 20, 1, np.where(data['%J'] > 80, -1, 0))
+
             def calculate_EMA(data):
                 data['EMA20'] = data['close'].ewm(span=20, adjust=False).mean()
                 data['EMA120'] = data['close'].ewm(span=120, adjust=False).mean()
                 data['EMA Signal'] = np.where(data['EMA20'] > data['EMA120'], 1, -1)
+
             def calculate_indicators(data):
                 calculate_macd(data)
                 calculate_rsi(data)
-                calculate_sma(data)
-                calculate_bollinger_bands(data)
                 calculate_EMA(data)
-            def generate_signals(data):
-                data['Signal'] = data[['MACD Signal', 'RSI Signal', 'SMA Signal', 'BB Signal','EMA Signal']].sum(axis=1)
+                calculate_kdj(data)
 
+            def generate_signals(data):
+                data['Signal'] = data[['MACD Signal', 'KDJ Signal', 'RSI Signal', 'EMA Signal']].sum(axis=1)
+
+            # Teknik göstergeleri hesapla ve sinyalleri oluştur
             calculate_indicators(data)
             generate_signals(data)
-            if data['Signal'].iloc[-1] >=beklenen_sinyal:
+
+            if data['Signal'].iloc[-1] >= 1:
                 data.fillna(0, inplace=True)
+
                 def create_lagged_features(data, lag=süre):
                     df = data.copy()
                     for i in range(1, lag + 1):
@@ -212,27 +201,23 @@ def newthe(liste):
 
                 data_with_lags = create_lagged_features(data)
                 data_with_lags.dropna(inplace=True)
-                X = data_with_lags.drop(['close', 'Signal Line', 'MACD Signal', 'RSI Signal',
-                                         'SMA Signal','BB Signal', 'EMA Signal', 'Signal'],axis=1)
+                X = data_with_lags.drop(['Datetime', 'close', 'Signal Line', 'MACD Signal', 'RSI Signal',
+                                         'KDJ Signal', 'EMA Signal', 'Signal'], axis=1)
                 y = data_with_lags['close']
                 X_train, X_test, y_train, y_test = X.iloc[:-süre], X.iloc[-süre:], y.iloc[:-süre], y.iloc[-süre:]
-                from catboost import CatBoostRegressor
-                from sklearn.svm import SVR
-                from sklearn.linear_model import LinearRegression, Ridge, Lasso
-                from sklearn.neighbors import KNeighborsRegressor
-                estimators1={10,100,50,150,200}
+
+                estimators1 = [10, 100, 50, 150, 200]
                 for estimators in estimators1:
                     models = {
-                        'rf': RandomForestRegressor(n_estimators=estimators, random_state=int(ortalama)),
+                        'rf': RandomForestRegressor(n_estimators=estimators, random_state=42),
                         'gb': GradientBoostingRegressor(n_estimators=estimators, learning_rate=0.1, max_depth=3,
-                                                        random_state=int(ortalama)),
-                        'ab': AdaBoostRegressor(n_estimators=estimators, learning_rate=1.0, random_state=int(ortalama)),
-                        'xgb': XGBRegressor(n_estimators=estimators, learning_rate=0.1, max_depth=6, random_state=int(ortalama),
+                                                        random_state=42),
+                        'ab': AdaBoostRegressor(n_estimators=estimators, learning_rate=1.0, random_state=42),
+                        'xgb': XGBRegressor(n_estimators=estimators, learning_rate=0.1, max_depth=6, random_state=42,
                                             verbosity=0),
-                        'lgbm': LGBMRegressor(n_estimators=estimators, learning_rate=0.05, random_state=int(ortalama), verbosity=-1)
+                        'lgbm': LGBMRegressor(n_estimators=estimators, learning_rate=0.05, random_state=42,
+                                              verbosity=-1)
                     }
-
-                from sklearn.model_selection import cross_val_score
 
                 # Modelleri ve performanslarını depolamak için bir sözlük
                 model_performance = {}
@@ -244,7 +229,7 @@ def newthe(liste):
 
                 # Performansları karşılaştırın
                 sorted_models = sorted(model_performance.items(), key=lambda x: x[1])
-                best_models = sorted_models[:5]  # En iyi 5 modeli seçin
+                best_models = sorted_models[:2]
 
                 # Seçilen modellerle ensemble modeli oluşturun
                 best_model_objects = [(name, models[name]) for name, _ in best_models]
@@ -252,9 +237,6 @@ def newthe(liste):
 
                 # Ensemble modelini eğitin
                 ensemble_model.fit(X_train, y_train)
-                # Ensemble modelini eğit
-                ensemble_model.fit(X_train, y_train)
-
 
                 future_predictions = []
                 last_known_features = X_test.iloc[-1].values
@@ -297,8 +279,8 @@ def newthe(liste):
                     else:
                         print(Fore.RED + symbol+" yetersiz .....değişim : "+str(seventh_day_prediction-data[['close']].tail(1).values[0][0]) + Style.RESET_ALL)
                 else:
-                    print(Fore.RED + "BEKLENEN RMSE: " + str(beklenen_MAPE) + Style.RESET_ALL)
-                    print(Fore.RED + symbol + " YÜKSEK RMSE: " + str(MAPE) + Style.RESET_ALL)
+                    print(Fore.RED + "BEKLENEN MAPE: " + str(beklenen_MAPE) + Style.RESET_ALL)
+                    print(Fore.RED + symbol + " YÜKSEK MAPE: " + str(MAPE) + Style.RESET_ALL)
             elif data['Signal'].iloc[-1] <=-3:
                 print(Fore.RED + symbol + " Düşecek!!! " + Style.RESET_ALL)
 
@@ -309,16 +291,22 @@ def newthe(liste):
             else:
                 print(Fore.RED + symbol + " sinyal gücü!  : " +str(data['Signal'].iloc[-1])+ Style.RESET_ALL)
 
-
         except Exception as e:
-            print(e)
+            print(Fore.RED + str(e) + Style.RESET_ALL)
 
     file='buy_list.xlsx'
     old_data=pd.read_excel(file)
     sepet=sepet.sort_values(by='difference', ascending=False)
     sepet.dropna()
     old_data.dropna()
-    buylist = pd.concat([old_data, sepet], ignore_index=True)
+    if sepet.empty:
+        buylist = old_data.copy()  # Avoid modifying old_data
+    else:
+        # Check for columns with only NA values in sepet (optional)
+        na_cols = sepet.columns[sepet.isna().all()]
+        if len(na_cols) > 0:
+            sepet.drop(na_cols, axis=1, inplace=True)  # Drop NA columns (optional)
+        buylist = pd.concat([old_data, sepet], ignore_index=True)
     send_telegram_message("Buy sinyali verenler: " )
     send_telegram_message(str(sepet))
     send_telegram_message("Sell sinyali verenler: " + str(sell))
@@ -332,8 +320,12 @@ def newthe(liste):
     send_telegram_file('buy_list.xlsx')
 
 
-while True:
-      newthe(liste)
-      print("bitti...")
-      time.sleep(60*60*24)
+if st.button("Analiz"):
+    while True:
+        baslangıc=datetime.now()
+        newthe(liste)
+        print("bitti...")
+        print("Analiz süresi:",str(datetime.now()-baslangıc))
+        send_telegram_message("Analiz süresi: " + str(datetime.now()-baslangıc))
+        time.sleep(60*60)
 
